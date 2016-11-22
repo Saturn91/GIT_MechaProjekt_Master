da3a0044c54dddf66d0f9106618bcbb7b0245646
Saturn91 <manuel.geissberger@gmail.com>
2016-11-22 19:45:40 +0100

    Revert "[BUG] Lib PI4J is not defined in jar!"
    This reverts commit a962ac5bbeddc5a4f28aee949635aaa5edb18300.

--- TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
+++ TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
@@ -1,6 +1,10 @@
 package com.EnergyHarvesting.Master.TestProject;
 
-import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.serial.RaspberryPiSerial;
 
 /**
  * Testapp to check Pins
@@ -9,70 +13,20 @@ import com.pi4j.io.gpio.*;
  */
 public class App 
 {
+	private static GpioPinDigitalInput button1;
+	private static GpioController gpio = GpioFactory.getInstance();
 	
     public static void main( String[] args )
     {
-    	System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-
-        // create gpio controller
-        //final GpioController gpio = GpioFactory.getInstance();
-    	final GpioController gpio = null;
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-
-        // set shutdown state for this pin
-        pin.setShutdownOptions(true, PinState.LOW);
-
-        System.out.println("--> GPIO state should be: ON");
-
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-        // turn off gpio pin #01
-        pin.low();
-        System.out.println("--> GPIO state should be: OFF");
-
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
-
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-        // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
-
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
-
-        System.out.println("Exiting ControlGpioExample");
+        button1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07);
+        if(button1.isHigh()){
+        	System.out.println("is High");
+        }else{
+        	if(button1.isLow()){
+        		System.out.println("is Low");
+        	}else{
+        		System.out.println("no data!");
+        	}        	
+        }
     }
 }
