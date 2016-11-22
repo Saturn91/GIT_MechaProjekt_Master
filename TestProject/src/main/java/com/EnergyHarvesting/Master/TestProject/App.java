6e8fc7c1dc3919bc328c3a17761f0534bffdb5d1
Saturn91 <manuel.geissberger@gmail.com>
2016-11-22 20:50:22 +0100

    [BUG_FIX] Can now use Lib
    Dependencies are now loaded by maven

--- TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
+++ TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
@@ -4,7 +4,6 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.serial.RaspberryPiSerial;
 
 /**
  * Testapp to check Pins
