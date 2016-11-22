3deaf06e2c1373d933c5845972869be541511ce9
Saturn91 <manuel.geissberger@gmail.com>
2016-11-22 14:31:19 +0100

    [ADD] simple Testprogramm
    this simple testprogramm checks if a button on Pin 7 is pressed or not

--- TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
+++ TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/App.java
@@ -1,13 +1,32 @@
 package com.EnergyHarvesting.Master.TestProject;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.serial.RaspberryPiSerial;
+
 /**
- * Hello world!
+ * Testapp to check Pins
+ * @author M.Geissbberger
  *
  */
 public class App 
 {
+	private static GpioPinDigitalInput button1;
+	private static GpioController gpio = GpioFactory.getInstance();
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        button1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07);
+        if(button1.isHigh()){
+        	System.out.println("is High");
+        }else{
+        	if(button1.isLow()){
+        		System.out.println("is Low");
+        	}else{
+        		System.out.println("no data!");
+        	}        	
+        }
     }
 }
