c42b1d6b9ec7b70687ee44550e8a8af442da1730
Saturn91 <manuel.geissberger@gmail.com>
2016-11-28 15:45:54 +0100

    [EDIT] Maxline in GUI-CMD_Output was to much

--- TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/gui/MainPanel.java
+++ TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/gui/MainPanel.java
@@ -38,7 +38,6 @@ public class MainPanel extends PanelComponent{
 		setLayout(null);
 		graphPanel = new GraphPanel(0, 0, 1350, 540);
 		settingPanel = new SettingPanel(0, 540, 900, 360);
-		cmd_Panel = new CMD_Panel(900, 540, 450, 360, 25);	//should be: 900, 540, 450, 360 (25 = maxLines)
 		
 		add(graphPanel);
 		graphPanel.init();
