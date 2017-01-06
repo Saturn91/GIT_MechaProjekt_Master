5c06bb988ce51f716b7626e844d85e11b4ebdbff
Saturn91 <manuel.geissberger@gmail.com>
2017-01-06 20:18:56 +0100

    [ADD] change range of Graph automatic

--- TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/gui/subPanel/GraphPanel.java
+++ TestProject/src/main/java/com/EnergyHarvesting/Master/TestProject/gui/subPanel/GraphPanel.java
@@ -220,6 +220,13 @@ public class GraphPanel extends PanelComponent{
 					float seconds = Calendar.getInstance().get(Calendar.SECOND);
 					float time = hour + minutes/60 + seconds/3600;
 					int index = sensors[i].getTemperatur().size()-1;
+					//for first point adjust time
+					if(index == 1){
+						temperature.setMinValueX(time-0.01f);
+						temperature.setMaxValueX(time+0.25f);
+						voltage.setMinValueX(time-0.01f);
+						voltage.setMaxValueX(time+0.25f);
+					}
 					temperature.addPoint(i, time , sensors[i].getTemperatur().get(index));
 					voltage.addPoint(i, time , sensors[i].getVoltage().get(index));
 					temperature.addGraphName(i, sensors[i].getName());
@@ -377,6 +384,17 @@ public class GraphPanel extends PanelComponent{
 					float minutes = calender.get(Calendar.MINUTE);
 					float seconds = Calendar.getInstance().get(Calendar.SECOND);
 					float time = hour + minutes/60 + seconds/3600;
+					//for first point adjust time
+					if(index == 1){
+						temperature.setMinValueX(time-0.01f);
+						voltage.setMinValueX(time-0.01f);
+						
+					}					
+					//for last adjust maxTime
+					if(index == sensors[i].getVoltage().size()-1){
+						temperature.setMaxValueX(time+0.01f);
+						voltage.setMaxValueX(time+0.01f);
+					}
 					temperature.addPoint(i, time , sensors[i].getTemperatur().get(index));
 					voltage.addPoint(i, time , sensors[i].getVoltage().get(index));
 				}				
